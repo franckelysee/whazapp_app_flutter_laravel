12051ced870b63bae6f1e51ba52d5355631886aa
Franck ELysee <f.elysee@toolbrothers.com>
2024-04-10 10:37:51 +0100

    calendar

--- lib/screens/booking_page.dart
+++ lib/screens/booking_page.dart
@@ -31,15 +31,81 @@ class _BookingPageState extends State<BookingPage> {
         icon: const FaIcon(Icons.arrow_back_ios),
       ),
       body: CustomScrollView(
+        scrollDirection: Axis.vertical,
+        clipBehavior: Clip.none,
         slivers: <Widget>[
           SliverToBoxAdapter(
             child: Column(
               children: <Widget>[
                 //display table calendar
                 _tableCalendar(),
-                const Padding(padding: EdgeInsets.symmetric(vertical: 25, horizontal: 15)),
+                const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 25, horizontal: 15),
+                  child: Center(
+                    child: Text(
+                      'Select Consultation Time',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold
+                      ),
+                    ),
+                  ),
+                ),
               ],
             ),
+          ),
+          _isWeekend ? SliverToBoxAdapter(
+            child: Container(
+              padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 15), 
+              child: const Text(
+                'Sorry, we are not available on weekends\nPlease enter a date that is not on a weekend',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.red
+                ),
+              ),
+            ),
+          ): SliverGrid(
+            delegate: SliverChildBuilderDelegate((context, index) {
+              return InkWell(
+                splashColor: Colors.transparent,
+                onTap: () {
+                  setState(() {
+                    _currentIndex = index;
+                    _timeSelected = true;
+                  });
+                },
+                child: index +9 <= 17 ? Container(
+                  margin: const EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: _currentIndex == index 
+                      ? Colors.white 
+                      : Colors.black
+                    ),
+                    borderRadius: BorderRadius.circular(15),
+                    color: _currentIndex == index ? Config.primaryColor : null,
+
+                  ),
+                  alignment: Alignment.center,
+                  child: Text(
+                    '${index + 9}:00 ${index +9 > 11 ? 'PM' : 'AM'}',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: _currentIndex == index ? Colors.white : null
+                    ),
+                  ),
+                ): null,
+              );
+            }),
+
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 4,
+              childAspectRatio: 1.5
+            ),
+          
           )
         ],
       ),
@@ -50,8 +116,8 @@ class _BookingPageState extends State<BookingPage> {
   Widget _tableCalendar() {
     return TableCalendar(
       focusedDay: _focusDay,
-      firstDay: DateTime.now(),
-      lastDay: DateTime(2023,12,31),
+      firstDay: DateTime.utc(1969,1,1),
+      lastDay: DateTime.utc(2024, 5, 31),
       calendarFormat: _format,
       currentDay: _currentDay,
       rowHeight: 40,
